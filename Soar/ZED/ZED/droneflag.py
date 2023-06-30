aa427154ef270a31bae4d1527859393c9a0cca6d
ivory234 <54278901+ivory234@users.noreply.github.com>
2023-06-30 20:07:31 +0900

    Add files via upload

---
+++ Soar/ZED/ZED/droneflag.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+class Flags(Enum):
+    
+    state_missionOnGoing = False
+    state_flag_arm = False
+    tate_flag_camerisOp = False
+    
+    
+    #--------------------------
+    
+    
+    mission_take_off_flag = 4
+    mission_wondering_flag =5
+    mission_find_obs_flag = 6
+        
+    
+        
+        
+        
+        
+        
+
+        
+        
+        
+        
+        
+    
+    
+    
